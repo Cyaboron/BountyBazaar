03e3d9b1d9c897160f71b1430434cf5c72fdd665
Cyaboron <cyaboron@gmail.com>
2025-02-27 09:20:19 +0100

    First commit

---
+++ Source/BountyBazaar/BountyBazaar.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BountyBazaar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BountyBazaar, "BountyBazaar" );
+ 
---
+++ Source/BountyBazaar/BountyBazaarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BountyBazaarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABountyBazaarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABountyBazaarGameMode();
+};
+
+
+
