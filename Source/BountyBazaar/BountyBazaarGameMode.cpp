cdbedd24b8613c57c69ff021008865f249e71659
Cyaboron <cyaboron@gmail.com>
2025-04-23 15:28:03 +0200

    Fix

--- Source/BountyBazaar/BountyBazaarGameMode.cpp
+++ Source/BountyBazaar/BountyBazaarGameMode.cpp
@@ -7,9 +7,9 @@
 ABountyBazaarGameMode::ABountyBazaarGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+//	// set default pawn class to our Blueprinted character
+//	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+//	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
 
--- Source/BountyBazaar/FInventoryItemDefinition.cpp
+++ Source/BountyBazaar/FInventoryItemDefinition.cpp
@@ -1,2 +1,1 @@
-﻿#include "D:\Syntra 2024_2025_Deel2\Eindproject\Git Clone\Intermediate\Build\Win64\x64\BountyBazaarEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "FInventoryItemDefinition.h"
+﻿#include "FInventoryItemDefinition.h"
